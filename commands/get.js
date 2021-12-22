5c41c51019ddd414171b09e99e8ff25838b379cc
Skunka <quentin.somveille3874@gmail.com>
2021-12-23 00:54:05 +0100

    feat: fc-get command

--- commands/get.js
+++ commands/get.js
@@ -3,35 +3,66 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { apiToken } = require("../config.json");
 const rp = require("request-promise");
-const requestOptions = {
-  method: "GET",
-  uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-  qs: {
-    start: "1",
-    limit: "5000",
-    convert: "USD",
-  },
-  headers: {
-    "X-CMC_PRO_API_KEY": `${apiToken}`,
-  },
-  json: true,
-  gzip: true,
-};
+const DEFAULT_CONVERT = "USD";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("fc-get")
     .setDescription(
       "Replies with current value of specified crypto pairing (default on ---/usdt)"
+    )
+    .addStringOption((option) =>
+      option
+        .setName("symbol")
+        .setDescription(
+          "Insert a crypto currency symbol like 'btc' or with pairing 'btc/usdt'. Default pairing: usdt "
+        )
+        .setRequired(true)
     ),
   async execute(interaction) {
+    // Deconstruct command option Symbol
+    const pairing = interaction.options
+      .getString("symbol")
+      .trim()
+      .toUpperCase()
+      .split("/");
+
+    // Define request query parameters
+    const symbol = pairing[0];
+    const convert = !pairing[1] ? DEFAULT_CONVERT : pairing[1];
+    //: Build request to CoinMarketCap API
+    const requestOptions = {
+      method: "GET",
+      uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest",
+      qs: {
+        symbol: symbol,
+        convert: convert,
+      },
+      headers: {
+        "X-CMC_PRO_API_KEY": `${apiToken}`,
+      },
+      json: true,
+      gzip: true,
+    };
+
+    //: Request Crypto Market Cap Api
     await rp(requestOptions)
       .then((response) => {
-        console.log("API call response:", response);
-        interaction.reply("Look console :) ");
+        // Get currency information
+        const currency = response["data"][symbol];
+        const quote = currency["quote"][convert];
+
+        const name = currency["name"];
+        const price = quote["price"].toFixed(5);
+        const percent = quote["percent_change_1h"].toFixed(2);
+
+        interaction.reply(`${name}: ${price} ${convert} | ${percent}%`);
       })
       .catch((err) => {
         console.log("API call error:", err.message);
+        interaction.reply(
+          "There was an error while fetching currency informations. \nPlease check that you entered a existing currency / pairing."
+        );
       });
   },
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
-const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
+const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -9,36 +9,40 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
 // Read commands folder
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 // Register commands
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Forge | Crypto is ready to work !');
+client.once("ready", () => {
+  console.log("Forge | Crypto is ready to work !");
 });
 
 // Listen on new interaction
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-    const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-    if (!command) return;
+  if (!command) return;
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 // Login to Discord with your client's token
