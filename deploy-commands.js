28f5a9840768d7b5922888e0c3ead592e82d0f68
Quentin Somveille <quentin.somveille@axopen.com>
2021-12-22 17:03:12 +0100

    feat: basics

---
+++ deploy-commands.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Forge | Crypto commands are now available for use !'))
+    .catch(console.error);
