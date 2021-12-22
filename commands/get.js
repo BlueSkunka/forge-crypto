70d90c93ffd88f261fad3de57c29bafe679b8df1
Skunka <quentin.somveille3874@gmail.com>
2021-12-22 19:10:15 +0100

    feat: init coin market cap api

--- commands/get.js
+++ commands/get.js
@@ -1,13 +1,37 @@
 //: /get {pair}
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { apiToken } = require('../config.json');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { apiToken } = require("../config.json");
+const rp = require("request-promise");
+const requestOptions = {
+  method: "GET",
+  uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+  qs: {
+    start: "1",
+    limit: "5000",
+    convert: "USD",
+  },
+  headers: {
+    "X-CMC_PRO_API_KEY": `${apiToken}`,
+  },
+  json: true,
+  gzip: true,
+};
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('fc-get')
-        .setDescription('Replies with current value of specified crypto pairing (default on ---/usdt)'),
-    async execute(interaction) {
-        await interaction.reply('WIP');
-    },
+  data: new SlashCommandBuilder()
+    .setName("fc-get")
+    .setDescription(
+      "Replies with current value of specified crypto pairing (default on ---/usdt)"
+    ),
+  async execute(interaction) {
+    await rp(requestOptions)
+      .then((response) => {
+        console.log("API call response:", response);
+        interaction.reply("Look console :) ");
+      })
+      .catch((err) => {
+        console.log("API call error:", err.message);
+      });
+  },
 };
