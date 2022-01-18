8e383d3ccd2046d5e284b27d0de42fdfd841ca5d
Quentin Somveille <quentin.somveille@axopen.com>
2022-01-18 21:12:21 +0100

    feat: sync command

--- commands/sync.js
+++ commands/sync.js
@@ -1,11 +1,31 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { execute } = require("./get");
+const rp = require("request-promise");
+const COINGECKO_API = "https://api.coingecko.com/api/v3/coins/list";
+const FileModule = require("./../modules/file.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("fc-sync")
     .setDescription("Sync currencies mock file with coin gecko"),
   async execute(interaction) {
-    interaction.reply();
+    await rp({
+      method: "GET",
+      uri: COINGECKO_API,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      json: true
+    })
+      .then((response) => {
+        FileModule.WriteFile("currencies.json", JSON.stringify(response, null, 2));
+
+        interaction.reply("List of all crpto currency has been updated !");
+      }).catch((err) => {
+        console.error(err);
+
+        interaction.reply("Error while fetching crypto currencies data. Please retry later or contact bot maintainer (see help command)");
+      });
+
+
   },
 };
