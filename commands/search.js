14c3272e2b62df40e69203237a05ffff6e72892f
Skunka <quentin.somveille3874@gmail.com>
2022-01-20 00:53:07 +0100

    feat: search command

--- commands/search.js
+++ commands/search.js
@@ -1,25 +1,48 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const FileModule = require("./../modules/file.js");
 const filePath = "currencies.json";
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("fc-search")
-        .setDescription("Search for currency name from symbol")
-        .addStringOption((option) => {
-            option.setName("Symbol")
-                .setRequired(true)
-        }),
-    async execute(interaction) {
-        const currencies = FileModule.ReadFile(filePath);
+  data: new SlashCommandBuilder()
+    .setName("fc-search")
+    .setDescription("Search for currency name from symbol")
+    .addStringOption((option) =>
+      option.setName("symbol").setDescription("Search for currency info")
+    ),
+  async execute(interaction) {
+    const currencies = FileModule.ReadFile(filePath);
 
-        if (null == currencies) {
-            interaction.reply("Currencies file isn't up to date, please use sync command.");
-            return;
-        }
-
-        let symbol = interaction.options.getString("symbol");
-        console.info(currencies);
-        interaction.reply("Work In Progress.");
+    if (null == currencies) {
+      interaction.reply(
+        "Currencies file isn't up to date, please use sync command."
+      );
+      return;
     }
-}
+
+    let symbol = interaction.options.getString("symbol");
+    let hasMatched;
+    let matchedCurrencies = [];
+
+    for (let i = 0; i < Object.keys(currencies).length; i++) {
+      if (
+        currencies[i]["symbol"] == symbol ||
+        currencies[i]["id"] == symbol ||
+        currencies[i]["name"] == symbol
+      ) {
+        matchedCurrencies.push(currencies[i]);
+        hasMatched = true;
+      }
+    }
+
+    if (!hasMatched) {
+      interaction.reply("Not match found :(");
+    } else {
+      let response = JSON.stringify(matchedCurrencies);
+      response = response
+        .replace(/\[\{/g, "Correspondances: ")
+        .replace(/\}\]/g, "")
+        .replace(/\}\,\{/g, " \\n ");
+      interaction.reply(response);
+    }
+  },
+};
