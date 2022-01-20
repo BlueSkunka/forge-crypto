f5cc3a049de99c08547fd2a80c921901e39f6eb8
Quentin Somveille <quentin.somveille@axopen.com>
2022-01-20 16:28:01 +0100

    fix: use __dirname as base path for files

--- modules/file.js
+++ modules/file.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const basePath = __dirname.concat("/../");
 
 module.exports = class FileModule {
   static ReadFile(filePath, jsonParse = true) {
     try {
-      let data = fs.readFileSync(filePath, "utf-8");
+      let data = fs.readFileSync(basePath.concat(filePath), "utf-8");
       return jsonParse ? JSON.parse(data) : data;
     } catch (error) {
       console.error(error);
@@ -13,7 +14,7 @@ module.exports = class FileModule {
 
   static WriteFile(filePath, data) {
     try {
-      fs.writeFileSync(filePath, data);
+      fs.writeFileSync(basePath.concat(filePath), data);
       return true;
     } catch (err) {
       console.error(err);
