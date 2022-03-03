47e5fc144d65640a9ab6e5b07366d5844dbd5d31
Quentin Somveille <quentin.somveille@axopen.com>
2022-03-03 23:06:43 +0100

    doc: comment file module

--- modules/file.js
+++ modules/file.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 const basePath = __dirname.concat("/../");
 
 module.exports = class FileModule {
+  /**
+   * Read file data then return it or null if any error happens
+   * @param {*} filePath string
+   * @param {*} jsonParse boolean
+   * @returns {} | null
+   */
   static ReadFile(filePath, jsonParse = true) {
     try {
       let data = fs.readFileSync(basePath.concat(filePath), "utf-8");
@@ -12,6 +18,12 @@ module.exports = class FileModule {
     }
   }
 
+  /**
+   * Write file data
+   * @param {*} filePath string
+   * @param {*} data {}
+   * @returns boolean
+   */
   static WriteFile(filePath, data) {
     try {
       fs.writeFileSync(basePath.concat(filePath), data);
