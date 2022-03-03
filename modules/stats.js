b2d6c24c389619edd36a3a22c26f432bb1d210a5
Quentin Somveille <quentin.somveille@axopen.com>
2022-03-03 21:45:11 +0100

    refactor: stat module

--- modules/stats.js
+++ modules/stats.js
@@ -5,35 +5,61 @@ const commandFile = "stats/commands.json";
 
 module.exports = class StatModule {
   //#region Commands
-  static BuildNewCommand(failed) {
+  /**
+   * Create a command skeleton
+   * @returns {}
+   */
+  static BuildNewCommand() {
     return {
       hits: 1,
-      success: +!failed, // TODO: Maybe not understandable
-      failed: +failed,
+      success: 0,
+      failed: 0,
     };
   }
 
-  static CountCommand(commandName, failed) {
-    let data = FileModule.ReadFile(commandFile);
-
+  /**
+   * Control stats file skeleton and create required part
+   * May be exploded if more sections have to be registered
+   * @returns {}
+   */
+  static ControlSkeletonCommand(commandName, data) {
+    // File not found - create file skeleton
     if (null === data) {
       data = {};
       data["commands"] = {};
-      data["commands"][commandName] = this.BuildNewCommand(failed);
+      data["commands"][commandName] = this.BuildNewCommand();
     }
 
+    // Commands section not registered yet - Create commands section skeleton
     if (!data["commands"]) {
       data["commands"] = {};
     }
 
-    if (!data["commands"][commandName]) { // TODO: Add comments
-      data["commands"][commandName] = this.BuildNewCommand(failed);
-    } else {
-      data["commands"][commandName]["hits"]++;
-      if (failed) data["commands"][commandName]["failed"]++;
-      else data["commands"][commandName]["success"]++;
+    // Command not registered yet - Create command skeleton
+    if (!data["commands"][commandName]) {
+      data["commands"][commandName] = StatModule.BuildNewCommand();
     }
 
+    return data;
+  }
+
+  /**
+   * Rewrite stat file with updated command count
+   * May be transfered to database service if use cases increase
+   * @param {*} commandName string
+   * @param {*} failed boolean
+   */
+  static CountCommand(commandName, failed) {
+    let data = StatModule.ControlSkeletonCommand(commandName, FileModule.ReadFile(commandFile));
+
+    // Increase command hit total count
+    data["commands"][commandName]["hits"]++;
+
+    // Increase command response statut count
+    if (failed) data["commands"][commandName]["failed"]++;
+    else data["commands"][commandName]["success"]++;
+
+    // Write updated stats to file
     FileModule.WriteFile(commandFile, JSON.stringify(data, null, 2));
   }
   //#endregion
